45ac4aa284b0aedf365d77baa28f1e7b2be6a666
sam-njuguna <sam.x.njuguna@gmail.com>
2023-04-05 17:24:36 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="">
+      <div className="">
+        <h2 className="text-red-600">App</h2>
+      </div>
+    </div>
+  );
+};
+
+export default App;
